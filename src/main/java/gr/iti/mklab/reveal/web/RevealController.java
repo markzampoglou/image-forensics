cd7feee55dd2c1306b04c58eaeb12c28e9e00d00
markzampoglou <markzampoglou@gmail.com>
2016-06-22 20:43:52 +0300

    final version for docker

--- src/main/java/gr/iti/mklab/reveal/web/RevealController.java
+++ src/main/java/gr/iti/mklab/reveal/web/RevealController.java
@@ -17,7 +17,7 @@ import javax.annotation.PreDestroy;
 public class RevealController {
 
       public RevealController() throws Exception {
-        Configuration.load(getClass().getResourceAsStream("/remote.properties"));
+        Configuration.load(getClass().getResourceAsStream("/docker.properties"));
        // MorphiaManager.setup(Configuration.MONGO_HOST);
     }
 
